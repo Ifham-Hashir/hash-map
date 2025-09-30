35d1784bba92216fa35b414574f16edba5038ff9
Ifham-Hashir <ifham09052003@gmail.com>
2025-09-30 21:41:02 +0530

    Add growth logic and remove function

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 function Node(key, value, nextNode = null) {
-  return {key, value, nextNode}
+  return { key, value, nextNode };
 }
 
-function LinkedList(){
+function LinkedList() {
   return {
     head: null,
     tail: null,
     length: 0,
     append(key, value) {
       let newNode = Node(key, value);
-      if (this.length === 0){
+      if (this.length === 0) {
         this.head = newNode;
-      }
-      else{
+      } else {
         this.tail.nextNode = newNode;
       }
       this.tail = newNode;
       this.length++;
     },
-  }
+  };
 }
 
 function HashMap() {
   let capacity = 16;
   let buckets = Array(capacity);
-  for(let i = 0; i < capacity; i++){
+  for (let i = 0; i < capacity; i++) {
     buckets[i] = LinkedList();
   }
   return {
@@ -32,10 +31,10 @@ function HashMap() {
     size: 0,
     capacity,
     buckets,
-    
+
     hash(key) {
       let hashCode = 0;
-      
+
       const primeNumber = 31;
       for (let i = 0; i < key.length; i++) {
         hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
@@ -45,15 +44,24 @@ function HashMap() {
     },
 
     set(key, value) {
-      let index = this.hash(key);
-  
-      if(this.buckets[index].head === null){
-        this.buckets[index].append(key,value);
+      //Growth Logic
+      if (this.size >= this.capacity * this.loadFactor) {
+        let keys = this.keys();
+        let values = this.values();
+        this.capacity *= 2;
+        this.clear();
+        for(let i = 0; i < keys.length; i++){
+          this.set(keys[i], values[i]);
+        }
       }
-      else{
+
+      let index = this.hash(key);
+      if (this.buckets[index].head === null) {
+        this.buckets[index].append(key, value);
+      } else {
         let currentNode = this.buckets[index].head;
-        while(currentNode !== null){
-          if(currentNode.key === key){
+        while (currentNode !== null) {
+          if (currentNode.key === key) {
             currentNode.value = value;
             return;
           }
@@ -61,18 +69,18 @@ function HashMap() {
         }
         this.buckets[index].append(key, value);
       }
-      
+
       this.size++;
     },
 
     get(key) {
       let index = this.hash(key);
 
-      if(this.buckets[index].head !== null){
+      if (this.buckets[index].head !== null) {
         let currentNode = this.buckets[index].head;
 
-        while(currentNode !== null){
-          if(currentNode.key === key){
+        while (currentNode !== null) {
+          if (currentNode.key === key) {
             return currentNode.value;
           }
           currentNode = currentNode.nextNode;
@@ -85,11 +93,11 @@ function HashMap() {
     has(key) {
       let index = this.hash(key);
 
-      if(this.buckets[index].head !== null){
+      if (this.buckets[index].head !== null) {
         let currentNode = this.buckets[index].head;
 
-        while(currentNode !== null){
-          if(currentNode.key === key){
+        while (currentNode !== null) {
+          if (currentNode.key === key) {
             return true;
           }
           currentNode = currentNode.nextNode;
@@ -99,28 +107,44 @@ function HashMap() {
       return false;
     },
 
-    remove(key){
+    remove(key) {
       let index = this.hash(key);
 
-      if(typeof(this.buckets[index]) !== "undefined"){
+      if (this.buckets[index].head !== null) {
         let currentNode = this.buckets[index].head;
-        let previousNode = currentNode;
-        if(this.buckets[index].length === 1){
+        let previousNode;
+        if (this.buckets[index].length === 1) {
           this.buckets[index].head = null;
           this.buckets[index].tail = null;
-        }
-        else{
-          while(currentNode !== null){
-            if(currentNode.key === key){
-              
+          this.buckets[index].length--;
+          this.size--;
+          return true;
+        } else {
+          while (currentNode !== null) {
+            if (
+              currentNode.key === key &&
+              this.buckets[index].head === currentNode
+            ) {
+              this.buckets[index].head = currentNode.nextNode;
+              this.size--;
+              this.buckets[index].length--;
               return true;
             }
+            previousNode = currentNode;
             currentNode = currentNode.nextNode;
-          }          
+            if (currentNode.key === key) {
+              if (currentNode === this.buckets[index].tail) {
+                this.buckets[index].tail = previousNode;
+              }
+              previousNode.nextNode = currentNode.nextNode;
+              this.size--;
+              this.buckets[index].length--;
+              return true;
+            }
+          }
         }
       }
-
-      return false;      
+      return false;
     },
 
     length() {
@@ -129,21 +153,20 @@ function HashMap() {
 
     clear() {
       this.buckets = Array(this.capacity);
-      for(let i = 0; i < this.capacity; i++){
+      for (let i = 0; i < this.capacity; i++) {
         this.buckets[i] = LinkedList();
-      }      
+      }
       this.size = 0;
     },
 
     keys() {
       let index = 0;
       let keysArray = [];
-      while (index < this.buckets.length){
-        if(this.buckets[index].head !== null){
-
+      while (index < this.buckets.length) {
+        if (this.buckets[index].head !== null) {
           let currentNode = this.buckets[index].head;
 
-          while(currentNode !== null){
+          while (currentNode !== null) {
             keysArray.push(currentNode.key);
             currentNode = currentNode.nextNode;
           }
@@ -157,10 +180,10 @@ function HashMap() {
       let keys = this.keys();
       let valuesArray = [];
 
-      for(let i = 0; i < keys.length; i++){
+      for (let i = 0; i < keys.length; i++) {
         let value = this.get(keys[i]);
 
-        if(value !== null){
+        if (value !== null) {
           valuesArray.push(value);
         }
       }
@@ -172,32 +195,35 @@ function HashMap() {
       let keys = this.keys();
       let values = this.values();
       let entriesArray = [];
-      for (let i = 0; i < keys.length; i++){
+      for (let i = 0; i < keys.length; i++) {
         entriesArray.push([keys[i], values[i]]);
       }
       return entriesArray;
     },
-  }
+  };
 }
 
-let map = HashMap();
-
-map.set("i", "ifham");
-map.set("h", "hashir");
-map.set("i", "messi")
-map.set("y", "yarn")
-
-// console.log(map.buckets)
-console.log(map.get("i"))
-console.log(map.get("k"))
-console.log(map.has("i"))
-console.log(map.has("k"))
-console.log(map.length())
-console.log(map.keys())
-console.log(map.values())
-console.log(map.entries())
+ const test = HashMap() // or HashMap() if using a factory
 
 
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+console.log(test.length())
+
+test.set('moon', 'silver')
+
+console.log(test.length())
 
 
 
